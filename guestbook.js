7c00ba6dccd7f33e721833bfcb9cd51a7d01d638
Eric Johnson <38968846+ejmedia3@users.noreply.github.com>
2023-03-26 09:05:25 -0400

    Update guestbook.js
    add apiurl and two endpoints

--- guestbook.js
+++ guestbook.js
@@ -1,22 +1,22 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/45715242-a020-4b78-9d07-47adb755dd07/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
-  // add a single guestbood entry
+  // add a single guestbook entry
   add(name, email, comment) {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/save-guestbook-entry-sequence.json`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
